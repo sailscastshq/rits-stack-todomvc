648610c63aa2a840f43a2ea0f1e3c58e65390089
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-09 13:03:31 +0100

    chore: add dev script

---
+++ assets/js/components/TodoFooter.js
@@ -0,0 +1,32 @@
+import { Link } from '@inertiajs/inertia-react'
+
+export default function TodoFooter({ todosCount }) {
+  return (
+    <footer className="w-full">
+      <section className="flex w-full justify-between space-x-4">
+        <p className="font-light">
+          {!todosCount ? 'No todos left' : `${todosCount} todos left`}
+        </p>
+        <ul className="flex justify-center space-x-4">
+          <li>
+            <Link href="/">All</Link>
+          </li>
+          <li>
+            <Link href="/?todos=undone">Undone</Link>
+          </li>
+          <li>
+            <Link href="/?todos=done">Done</Link>
+          </li>
+        </ul>
+        <Link
+          method="DELETE"
+          href="/todos"
+          as="button"
+          className="font-light hover:underline"
+        >
+          Clear completed
+        </Link>
+      </section>
+    </footer>
+  )
+}
--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -3,65 +3,9 @@ import { Link } from '@inertiajs/inertia-react'
 
 export default function Index({ name }) {
   return (
-    <div className="flex h-screen flex-col items-center justify-center space-y-8 text-center text-gray-700">
-      <h1>
-        A brand new
-        <span className="text-xl font-bold text-purple-400"> {name}</span> app
-        on Sails
-      </h1>
+    <main className="mt-10 flex h-screen flex-col items-center space-y-8 text-center text-gray-700">
+      <h1 class="text-5xl text-purple-700">todos</h1>
       <Counter />
-
-      <Link href="/example" className="text-purple-600 underline">
-        Go to example page
-      </Link>
-
-      <ul className="flex space-x-4 text-gray-400">
-        <li>
-          <a
-            href="https://sailsjs.com"
-            target="_blank"
-            className="hover:text-gray-600 hover:underline"
-          >
-            Sails
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://inertiajs.com"
-            target="_blank"
-            className="hover:text-gray-600 hover:underline"
-          >
-            Inertia
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://reactjs.org"
-            target="_blank"
-            className="hover:text-gray-600 hover:underline"
-          >
-            React
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://tailwindcss.com"
-            target="_blank"
-            className="hover:text-gray-600 hover:underline"
-          >
-            Tailwind CSS
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://webpack.org"
-            target="_blank"
-            className="hover:text-gray-600 hover:underline"
-          >
-            Webpack
-          </a>
-        </li>
-      </ul>
-    </div>
+    </main>
   )
 }
