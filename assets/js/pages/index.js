d54b75131ee97010497788c6a0c03968d0a126b1
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-09 13:04:45 +0100

    feat: scaffold Index component

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -1,11 +1,15 @@
-import Counter from '@/components/Counter'
+import TodoFooter from '@/components/TodoFooter'
+import TodoList from '@/components/TodoList'
+import TodoForm from '@/components/TodoForm'
 import { Link } from '@inertiajs/inertia-react'
 
-export default function Index({ name }) {
+export default function Index({ todos }) {
   return (
-    <main className="mt-10 flex h-screen flex-col items-center space-y-8 text-center text-gray-700">
-      <h1 class="text-5xl text-purple-700">todos</h1>
-      <Counter />
+    <main className="mx-auto mt-10 flex h-screen w-3/6 flex-col space-y-8 text-gray-700">
+      <h1 className="text-center text-5xl text-purple-700">todos</h1>
+      <TodoForm />
+      <TodoList todos={todos} />
+      <TodoFooter todosCount={todos.length} />
     </main>
   )
 }
