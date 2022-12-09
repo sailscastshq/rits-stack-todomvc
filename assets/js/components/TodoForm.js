319261d51379a5519b076a5aa683836b66966935
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-09 13:03:41 +0100

    feat: add TodoForm

---
+++ assets/js/components/TodoForm.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+export default function TodoForm() {
+  const [newTodo, setNewTodo] = useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    Inertia.post('/todos', newTodo)
+  }
+  function handleChange(event) {
+    console.log(event.target.value)
+    setNewTodo(event.target.value)
+  }
+  return (
+    <form className="w-full" onSubmit={handleSubmit}>
+      <input
+        autoFocus
+        className="w-full border-none text-xl text-purple-900 caret-purple-500 placeholder:text-gray-400/60 focus:outline-none"
+        type="text"
+        name="name"
+        onChange={handleChange}
+        value={newTodo}
+        placeholder="Learn Sails.js"
+      />
+    </form>
+  )
+}
