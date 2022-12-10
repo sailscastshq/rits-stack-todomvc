ea638c817724b78edebabc07fe2b811344ff86a2
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-10 09:56:08 +0100

    feat: create todos when Todo is empty

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,22 +9,23 @@
  * https://sailsjs.com/config/bootstrap
  */
 
-module.exports.bootstrap = async function() {
-
+module.exports.bootstrap = async function () {
   // By convention, this is a good place to set up fake data during development.
   //
   // For example:
   // ```
   // // Set up fake development data (or if we already have some, avast)
-  // if (await User.count() > 0) {
-  //   return;
-  // }
-  //
-  // await User.createEach([
-  //   { emailAddress: 'ry@example.com', fullName: 'Ryan Dahl', },
-  //   { emailAddress: 'rachael@example.com', fullName: 'Rachael Shaw', },
-  //   // etc.
-  // ]);
-  // ```
+  if ((await Todo.count()) > 0) {
+    return
+  }
 
-};
+  await Todo.createEach([
+    { name: 'Meet Ruben' },
+    { name: 'Learn Sails.js' },
+    { name: 'Build awesome apps on the RITS Stack' },
+    { name: 'Make a tweet' },
+    { name: 'Read a book' },
+    { name: 'Go to the gym' },
+  ])
+  // ```
+}
