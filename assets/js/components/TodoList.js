35182f84255d83f06bb5193f5ce855ce287961e5
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-10 13:56:21 +0100

    chore: conditionally render the ul

--- assets/js/components/TodoList.js
+++ assets/js/components/TodoList.js
@@ -7,36 +7,38 @@ export default function TodoList({ todos }) {
   }
 
   return (
-    <ul className="flex flex-col items-start justify-start space-y-4">
-      {todos?.map((todo, index) => (
-        <li
-          className="group flex w-full items-center space-x-2 text-xl"
-          key={index}
-        >
-          <input
-            type="checkbox"
-            checked={todo.completed}
-            className="h-6 w-6 accent-purple-500"
-            onChange={() => updateTodo(todo.id)}
-          />
-          <label
-            className={`flex-1 ${
-              todo.completed ? 'text-gray-400 line-through' : 'text-gray-600'
-            }  `}
+    todos.length > 0 && (
+      <ul className="flex flex-col items-start justify-start space-y-4">
+        {todos.map((todo, index) => (
+          <li
+            className="group flex w-full items-center space-x-2 text-xl"
+            key={index}
           >
-            {todo.name}
-          </label>
-          <Link
-            href={`/todos/${todo.id}`}
-            method="DELETE"
-            as="button"
-            title="Remove todo"
-            className="justify-self-end text-lg text-gray-400 opacity-0 hover:text-gray-600 group-hover:opacity-100"
-          >
-            &#10005;
-          </Link>
-        </li>
-      ))}
-    </ul>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              className="h-6 w-6 accent-purple-500"
+              onChange={() => updateTodo(todo.id)}
+            />
+            <label
+              className={`flex-1 ${
+                todo.completed ? 'text-gray-400 line-through' : 'text-gray-600'
+              }  `}
+            >
+              {todo.name}
+            </label>
+            <Link
+              href={`/todos/${todo.id}`}
+              method="DELETE"
+              as="button"
+              title="Remove todo"
+              className="justify-self-end text-lg text-gray-400 opacity-0 hover:text-gray-600 group-hover:opacity-100"
+            >
+              &#10005;
+            </Link>
+          </li>
+        ))}
+      </ul>
+    )
   )
 }
