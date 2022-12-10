7523b743c5c5f4c9a58c7470a4c133f10c7ed91e
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-10 09:55:51 +0100

    feat: add style for main

--- assets/js/pages/index.js
+++ assets/js/pages/index.js
@@ -4,11 +4,25 @@ import TodoForm from '@/components/TodoForm'
 
 export default function Index({ todos, undoneTodosCount }) {
   return (
-    <main className="mx-auto mt-10 flex h-screen w-full flex-col space-y-8 px-4 text-gray-700 sm:w-2/6">
-      <h1 className="text-center text-5xl text-purple-700">todos</h1>
-      <TodoForm />
-      <TodoList todos={todos} />
-      <TodoFooter undoneTodosCount={undoneTodosCount} />
-    </main>
+    <>
+      <main className="mx-auto mt-10 flex h-screen min-h-screen w-full flex-col space-y-8 px-4 text-gray-700 sm:w-2/6">
+        <h1 className="text-center text-5xl text-purple-700">todos</h1>
+        <TodoForm />
+        <TodoList todos={todos} />
+        <TodoFooter undoneTodosCount={undoneTodosCount} />
+      </main>
+      <footer className="flex justify-center space-x-4">
+        <p>Built on the RITS Stack</p>
+        <p>
+          <a
+            href="https://github.com/sailscastshq/rits-stack-todomvc"
+            target="_blank"
+            className="text-purple-800 underline"
+          >
+            View the source
+          </a>
+        </p>
+      </footer>
+    </>
   )
 }
