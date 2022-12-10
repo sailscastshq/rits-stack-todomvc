7f78458d1e7587d7fc4e0e734c36d251d70b0073
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-10 06:30:27 +0100

    feat: use optional chaining

--- assets/js/components/TodoList.js
+++ assets/js/components/TodoList.js
@@ -1,23 +1,22 @@
 export default function TodoList({ todos }) {
   return (
     <ul className="flex flex-col items-start justify-start space-y-4">
-      {todos &&
-        todos.map((todo, index) => (
-          <li className="space-x-2" key={index}>
-            <input
-              type="checkbox"
-              checked={todo.completed}
-              className="accent-purple-500"
-            />
-            <label
-              className={`${
-                todo.completed ? 'text-gray-400 line-through' : 'text-gray-700'
-              }  `}
-            >
-              {todo.name}
-            </label>
-          </li>
-        ))}
+      {todos?.map((todo, index) => (
+        <li className="space-x-2" key={index}>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            className="accent-purple-500"
+          />
+          <label
+            className={`${
+              todo.completed ? 'text-gray-400 line-through' : 'text-gray-700'
+            }  `}
+          >
+            {todo.name}
+          </label>
+        </li>
+      ))}
     </ul>
   )
 }
