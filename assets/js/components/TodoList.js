a4ce6cbb08cd9930d38f651b1da6c941d54c5360
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-10 06:21:38 +0100

    feat: update todolist

--- assets/js/components/TodoList.js
+++ assets/js/components/TodoList.js
@@ -1,11 +1,21 @@
 export default function TodoList({ todos }) {
   return (
-    <ul className="flex items-start justify-start">
+    <ul className="flex flex-col items-start justify-start space-y-4">
       {todos &&
         todos.map((todo, index) => (
           <li className="space-x-2" key={index}>
-            <input type="checkbox" className="rounded-full" />
-            <label>{todo.name}</label>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              className="accent-purple-500"
+            />
+            <label
+              className={`${
+                todo.completed ? 'text-gray-400 line-through' : 'text-gray-700'
+              }  `}
+            >
+              {todo.name}
+            </label>
           </li>
         ))}
     </ul>
